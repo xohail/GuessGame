e1c49ce731151f1540fb659699904951b4c52acf
Sohail <engr.sohail36@gmail.com>
2024-02-28 11:55:58 +1100

    fix static issue

--- GuessGame2/IDisplayInformation.cs
+++ GuessGame2/IDisplayInformation.cs
@@ -2,6 +2,6 @@ namespace GuessGame2;
 
 public interface IDisplayInformation
 {
-    void DisplayMessage(string message);
-    void DisplayMessage(string string1, string string2, string string3, string string4);
+    static void DisplayMessage(string message){}
+    static void DisplayMessage(string string1, string string2, string string3, string string4){}
 }
